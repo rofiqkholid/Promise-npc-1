3ac4ecb3d2a3e9533b8b66486aeef446db01573d
Rofiq Kholid <rofiqgege99@gmail.com>
2026-04-30 14:05:56 +0700

    feat: initialize Laravel application configuration with custom routing and middleware settings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,12 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->redirectGuestsTo(env('PORTAL_LOGIN_URL', 'http://localhost:8080/login'));
+        $middleware->redirectGuestsTo(env('PORTAL_LOGIN_URL', 'https://promise.summitadyawinsa.co.id/login'));
         $middleware->trustProxies(at: '*');
         $middleware->encryptCookies(except: [
             'promise_auth_session',
